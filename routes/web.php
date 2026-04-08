46b829c59b048047be1b9e2eee419c582c8bafc7
ssshaxvaleeva-cyber <ssshaxvaleeva@gmail.com>
2026-04-08 14:34:01 +0500

    CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\MainController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\TestController;
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,3 +28,20 @@ Route::get('/array', [MainController::class, 'showArray'])->name('array');
 Route::get('/shuffle', [MainController::class, 'shuffleArray'])->name('shuffle');
 Route::get('/sort',    [MainController::class, 'sortArray'])->name('sort');
 Route::get('/filter',  [MainController::class, 'filterArray'])->name('filter');
+
+Route::get('/products',[ProductController::class, 'index'])
+->name('products.index');
+
+Route::get('/products/create', [ProductController::class, 'create'])
+->name('products.create');
+Route::post('/products', [ProductController::class, 'store'])
+->name('products.store');
+
+Route::get('/products/{product}', [ProductController::class, 'show'])
+->name('products.show');
+
+Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+
+Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
+
+Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name ('products.destroy');
